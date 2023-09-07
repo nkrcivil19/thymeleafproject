3923ae4fac46106796168882299163c6238912d0
nkrcivil19 <nkrcivil19@gmail.com>
2023-09-07 22:24:12 +0530

    new class Test add

---
+++ src/main/java/employee/mngement/Test.java
@@ -0,0 +1,12 @@
+package employee.mngement;
+
+import java.util.Date;
+
+public class Test {
+
+	public void getMessage() {
+		System.out.println("hello");
+		System.out.println("new test on "+new Date());
+	}
+	
+}
